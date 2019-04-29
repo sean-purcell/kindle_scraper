006acaa7906c0ffeb49e7260697c50680ddd5f75
Sean Purcell <me@seanp.xyz>
2019-04-29 15:02:48 -0400

    Fix get_credentials

--- get_credentials.py
+++ get_credentials.py
@@ -3,18 +3,17 @@ import pickle
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly',
-          'https://www.googleapis.com/auth/gmail.send']
-
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send",
+]
 
 def main():
-    flow = InstalledAppFlow.from_client_secrets_file(
-	'project_credentials.json', SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file("project_credentials.json", SCOPES)
     creds = flow.run_local_server()
     # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-	pickle.dump(creds, token)
+    with open("token.pickle", "wb") as token:
+        pickle.dump(creds, token)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
