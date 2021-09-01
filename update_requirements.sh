0929b0aaa0ce47cbdeb845bb7724fb5390ca2e3e
Sean Purcell <me@seanp.xyz>
2021-08-31 21:59:12 -0400

    Update requirements.txt

--- update_requirements.sh
+++ update_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env/bin/pip freeze | egrep -v "pkg-resources|lxml|multidict|yarl" > requirements.txt
+env/bin/pip freeze | egrep -v "pkg-resources|lxml|multidict|yarl|six" > requirements.txt
