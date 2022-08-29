0b707c7251feb8ef02c24cf87c5376ba5a00edec
Sean Purcell <me@seanp.xyz>
2022-08-28 21:42:37 -0400

    Add pale lights scraper

---
+++ scraper/modules/pl.py
@@ -0,0 +1,31 @@
+import feedparser
+import time
+
+import scraper.util
+
+FEED_URL = "https://palelights.com/feed/"
+
+AUTHOR = "Erratic Errata"
+
+def _get_chapters(previous):
+    feed = feedparser.parse(FEED_URL)
+
+    return sorted(
+        filter(
+            lambda x: x[0] > previous,
+            [
+                (time.mktime(ent.published_parsed), ent.title, ent.content[0].value)
+                for ent in feed.entries
+            ],
+        )
+    )
+
+def scrape(state, _creds):
+    previous = state.get("previous", 0)
+    chapters = _get_chapters(previous)
+
+    out = []
+    for ts, title, text in chapters:
+        previous = max(previous, ts)
+        out.append(('Pale Lights: ' + title, scraper.util.format_chapter(title, text, AUTHOR)))
+    return (out, {"previous": previous})
